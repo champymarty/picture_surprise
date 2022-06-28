4e5bed25bca9251321631d96e68ea76027dbf8cb
Raphael <raphaelstjean@yahoo.ca>
2022-06-27 23:35:26 -0400

    fix token

--- constant.py
+++ constant.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 # load .env variables
 load_dotenv()
 
-guildIds = [833210288681517126] # test discord server
-# guildIds = None # force global commands
+# guildIds = [833210288681517126] # test discord server
+guildIds = None # force global commands
 
 temp_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "temp")
 
-TOKEN = os.getenv("TEST_TOKEN")
+TOKEN = os.getenv("TOKEN")
 ckey = "pictureSurprise"
 TENOR_KEY = os.getenv("tenor_key")
